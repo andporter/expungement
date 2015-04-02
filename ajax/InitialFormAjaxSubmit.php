d556fb74eea3717297fff90479473160ef5c59c6
andrew <andporter@gmail.com>
2015-04-01 20:02:54 -0600

    add all 12 quesitons to the ajax submit

--- ajax/InitialFormAjaxSubmit.php
+++ ajax/InitialFormAjaxSubmit.php
@@ -18,6 +18,16 @@ if (isset($_POST["data"])) {
         $tanfq2 = $jsonData['tanfq2'];
         $initialq1 = $jsonData['initialq1'];
         $initialq2 = $jsonData['initialq2'];
+        $initialq3 = $jsonData['initialq3'];
+        $initialq4 = $jsonData['initialq4'];
+        $initialq5 = $jsonData['initialq5'];
+        $initialq6 = $jsonData['initialq6'];
+        $initialq7 = $jsonData['initialq7'];
+        $initialq8 = $jsonData['initialq8'];
+        $initialq9 = $jsonData['initialq9'];
+        $initialq10 = $jsonData['initialq10'];
+        $initialq11 = $jsonData['initialq11'];
+        $initialq12 = $jsonData['initialq12'];
 
         require("../config/db.php");
         $db_connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
@@ -27,8 +37,8 @@ if (isset($_POST["data"])) {
         }
 
         if (!$db_connection->connect_errno) {
-            $sql = "INSERT INTO InitialFormStats (tanfq1, tanfq2, q1, q2)
-            VALUES('" . $tanfq1 . "', '" . $tanfq2 . "', '" . $initialq1 . "', '" . $initialq2 . "');";
+            $sql = "INSERT INTO InitialFormStats (tanfq1, tanfq2, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12)
+            VALUES('" . $tanfq1 . "', '" . $tanfq2 . "', '" . $initialq1 . "', '" . $initialq2 . "', '" . $initialq3 . "', '" . $initialq4 . "', '" . $initialq5 . "', '" . $initialq6 . "', '" . $initialq7 . "', '" . $initialq8 . "', '" . $initialq9 . "', '" . $initialq10 . "', '" . $initialq11 . "', '" . $initialq12 . "');";
 
             $query_insert = $db_connection->query($sql);
 
