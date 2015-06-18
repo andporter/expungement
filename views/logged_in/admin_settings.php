db76e4e83246e2508805460b58719f0e3b2138e4
andrew <andporter@gmail.com>
2015-06-17 19:28:07 -0600

    move delete user to the bottom of the settings page

--- views/logged_in/admin_settings.php
+++ views/logged_in/admin_settings.php
@@ -71,24 +71,6 @@ if (isset($registration))
     </div>
 </div>
 
-<div id="divDeleteUser" class="container theme-showcase">
-    <div class="panel panal-content panel-primary">
-        <div class="panel-heading">
-            <h3 class="panel-title"><span class="glyphicon glyphicon-cog"></span> Delete User: <em><?php echo $_SESSION['user_name']; ?></em></h3>
-        </div>
-        <div class="panel-body">
-            <div class="alert alert-danger" role="alert">
-                <strong>Warning: </strong>This will delete your account and you will be logged out. Please ensure you have another admin account you can use.
-            </div>
-            <form method="post" action="index.php?settings" name="registerform">
-                <label for="login_input_password">Account Password:</label>
-                <input id="login_input_password" class="login_input form-control" type="password" name="user_password" placeholder=" Account Password" pattern=".{6,}" required autocomplete="off" /><br>
-                <input type="submit"  name="deleteuser" value="Delete User" class="btn btn-danger pull-right" />
-            </form>
-        </div>
-    </div>
-</div>
-
 <div id="divRegisterUser" class="container theme-showcase">
     <div class="panel panal-content panel-primary">
         <div class="panel-heading">
@@ -105,6 +87,24 @@ if (isset($registration))
                 <label for="login_input_password_repeat">Repeat password:</label>
                 <input id="login_input_password_repeat" class="login_input form-control" type="password" name="user_password_repeat" placeholder=" Repeat Password" pattern=".{6,}" required autocomplete="off" /><br>
                 <input type="submit"  name="register" value="Register" class="btn btn-primary pull-right" />
+            </form>
+        </div>
+    </div>
+</div>
+
+<div id="divDeleteUser" class="container theme-showcase">
+    <div class="panel panal-content panel-primary">
+        <div class="panel-heading">
+            <h3 class="panel-title"><span class="glyphicon glyphicon-cog"></span> Delete User: <em><?php echo $_SESSION['user_name']; ?></em></h3>
+        </div>
+        <div class="panel-body">
+            <div class="alert alert-danger" role="alert">
+                <strong>Warning: </strong>This will delete your account and you will be logged out. Please ensure you have another admin account you can use.
+            </div>
+            <form method="post" action="index.php?settings" name="registerform">
+                <label for="login_input_password">Account Password:</label>
+                <input id="login_input_password" class="login_input form-control" type="password" name="user_password" placeholder=" Account Password" pattern=".{6,}" required autocomplete="off" /><br>
+                <input type="submit"  name="deleteuser" value="Delete User" class="btn btn-danger pull-right" />
             </form>
         </div>
     </div>
